6d99e642283bb978894a6cf726a69ef08ffe951e
MiscellaneousStuff <joe.makepeace01@gmail.com>
2021-07-13 07:22:19 +0100

    await send init msg

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class MusicMoverBot(discord.Client):
         )
 
         rythm_channel = self.get_channel(self.rythm_channel_id)
-        rythm_channel.send(f"{self.user} joined to clean this discord.")
+        await rythm_channel.send(f"{self.user} joined to clean this discord.")
 
     async def on_message(self, msg):
         """Intercepts Rythm bot (and optionally Rythm bot commands from users)
